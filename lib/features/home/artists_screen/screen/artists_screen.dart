a05f8dc0bc4ec4a193d7955d8489f654f0f97fc0
Saklain <saklain.flutter@gmail.com>
2025-10-28 06:08:34 +0600

    added artists screen

--- lib/features/home/artists_screen/screen/artists_screen.dart
+++ lib/features/home/artists_screen/screen/artists_screen.dart
@@ -7,7 +7,9 @@ import 'package:jconnect/core/common/constants/iconpath.dart';
 import 'package:jconnect/core/common/style/global_text_style.dart';
 import 'package:jconnect/core/common/widgets/custom_app_bar2.dart';
 import 'package:jconnect/features/home/artists_screen/controller/artists_controller.dart';
-import '../widgets/artists_item_list.dart';
+import 'package:jconnect/features/home/artists_screen/widgets/artists_item.dart';
+import '../widgets/artists_item_list_tab.dart';
+
 class ArtistsScreen extends StatelessWidget {
   const ArtistsScreen({super.key});
   @override
@@ -15,34 +17,35 @@ class ArtistsScreen extends StatelessWidget {
     var controller = Get.put(ArtistsController());
     return Scaffold(
       backgroundColor: AppColors.backGroundColor,
-      body: Obx(
-        () => Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 74.h),
-          child: Column(
-            children: [
-              CustomAppBar2(
-                title: "Artists",
-                actionIconUrl: Iconpath.notificationIcon,
-                actionOnTap: () {},
-                leadingIconUrl: Iconpath.backIcon,
-                onLeadingTap: () {
-                  Get.back();
-                },
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 74.h),
+        child: Column(
+          children: [
+            CustomAppBar2(
+              title: "Artists",
+              actionIconUrl: Iconpath.notificationIcon,
+              actionOnTap: () {},
+              leadingIconUrl: Iconpath.backIcon,
+              onLeadingTap: () {
+                Get.back();
+              },
+            ),
+            SizedBox(height: 30.h),
+            CustomTextfield(
+              controller: controller.searchTextController,
+              hintText: "Search artists or influencers…",
+              prefixIcon: Icon(
+                Icons.search,
+                size: sp(20),
+                color: AppColors.secondaryTextColor,
               ),
-              SizedBox(height: 30.h),
-              CustomTextfield(
-                controller: controller.searchTextController,
-                hintText: "Search artists or influencers…",
-                prefixIcon: Icon(
-                  Icons.search,
-                  size: sp(20),
-                  color: AppColors.secondaryTextColor,
-                ),
-              ),
-              SizedBox(height: 30.h),
-              ArtistsItemList(controller: controller),
-            ],
-          ),
+            ),
+            SizedBox(height: 30.h),
+            ArtistsItemListTab(controller: controller),
+            SizedBox(height: 40.h),
+
+            ArtistsItem(controller: controller),
+          ],
         ),
       ),
     );
